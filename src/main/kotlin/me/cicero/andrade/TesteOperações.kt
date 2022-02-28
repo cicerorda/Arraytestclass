002049118b9f258bb26657335d912112253d76e6
Dutch <cicero.rda@gmail.com>
2022-02-28 10:54:56 -0300

    created collection operations

--- src/main/kotlin/me/cicero/andrade/TesteOperações.kt
+++ src/main/kotlin/me/cicero/andrade/TesteOperações.kt
@@ -13,6 +13,19 @@ fun main() {
     println("Media salarial ${salarios.average()}")
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
-    println("-----------")
+    println("------------------")
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("------------------")
+    println(salarios.count {it in 2000.0..5000.0})
+
+    println("------------------")
+    println(salarios.find { it == 2250.0})
+    println(salarios.find { it == 500.0})
+
+    println("------------------")
+    println(salarios.any { it == 1000.0})
+    println(salarios.any { it == 500.0})
+
+
 }
